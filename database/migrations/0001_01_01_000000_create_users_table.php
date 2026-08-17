12cdd567c69cf3e737678e3b3de182b383af2d50
Haruzenn69 <mrizqibintangr@gmail.com>
2026-08-17 13:38:49 +0700

    perubahan & penambahan file migration(3)

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -12,7 +12,8 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->string('username')->primary();
+            $table->id();
+            $table->string('username')->unique();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
